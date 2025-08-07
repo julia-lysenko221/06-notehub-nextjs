b8dbb9658fd8de5cfcc3ea726a9cb462c1d9d9a1
juliya.lysenko <juliya.deleh@gmail.com>
2025-08-07 20:04:17 +0300

    commit1

---
+++ app/notes/page.tsx
@@ -0,0 +1,18 @@
+// import { fetchNotes } from '@/lib/api';
+// import NoteList from '@/components/NoteList/NoteList';
+
+// const Notes = async () => {
+//   const response = await fetchNotes();
+//   return;
+//   <section>
+//     <h1>Notes List</h1>
+//     {response?.notes?.length > 0 && <NoteList notes={response.notes} />}
+//   </section>;
+// };
+// export default Notes;
+
+import App from './Notes.client';
+
+export default function NotesPage() {
+  return <App />;
+}
